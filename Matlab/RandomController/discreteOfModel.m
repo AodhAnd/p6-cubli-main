3645bd4d928ec6fe5f3a1c545212c78f3e7bc62f
AodhAnd <julbrehin@gmail.com>
2016-04-10 15:35:20 +0200

    "Added bode plot generation for continuous and discrete controllers"

--- Matlab/RandomController/discreteOfModel.m
+++ Matlab/RandomController/discreteOfModel.m
@@ -6,17 +6,30 @@ T=1/100;%1/100;
 
 D=-4583.5*(s+9.483)*(s+1.599)/((s-5.539)*(s+100)*(s+200))/0.62737;
 
-Dz=c2d(D,T,'tustin');
-opts = c2dOptions('Method', 'tustin', 'PrewarpFrequency', 30.4);
-Dz2 = c2d(D,T,opts);
+Dz=c2d(D,T,'tustin')
+opts = c2dOptions('Method', 'tustin', 'PrewarpFrequency', 33);
+Dz2 = c2d(D,T,opts)
 
-%bodeplot(D,Dz);
+bodeplot(D,Dz,Dz2);
+% title('Step Response');
+% xlabel('Time (s)');
+% ylabel('Angular Position of the Frame (rad)');
+grid on;
+set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6);
+legend('Continuous controller','Discretized Controller (Tustin)','Discretized Controller (Tustin with Pre-Warping)','Location','Southeast');
 
 s=tf('s');
 G=(-J_w*s^2)/((J_w*s^2+B_w*s)*((J_f+m_w*l_w^2)*s^2+B_f*s-(m_w*l_w+m_f*l_f)*g+((B_w*J_w*s^3)/(J_w*s^2+B_w*s))));
 G_reduced=minreal(G); 
 
 Gz=c2d(G_reduced,T,'tustin');
-nyqlog(D*G_reduced);
-nyquist(Gz*Dz);
-rlocus(D*G);
+figure
+bodeplot(D*G_reduced/(1+D*G_reduced),Dz*Gz/(1+Dz*Gz),Dz2*Gz/(1+Dz2*Gz));
+grid on;
+set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6);
+legend('Continuous controller (CL)','Discretized Controller (Tustin)(CL)','Discretized Controller (Tustin with Pre-Warping)(CL)','Location','Southeast');
+
+
+%nyqlog(D*G_reduced);
+%nyquist(Gz*Dz);
+%rlocus(D*G);
