55cf2a4449bdb0ecd972a00d6af8c04fa073f9a9
Aand <julbrehin@gmail.com>
2016-05-13 17:01:02 +0200

    Factorized complementary filter
    Put the code into en external function residing in the Imu class

--- global/src/imu.cpp
+++ global/src/imu.cpp
@@ -14,13 +14,13 @@
 //This
 #include "../inc/imu.hpp"
 
-Imu::Imu(U8 i2cAddr,I2C* i2cObj)
-:
-mpI2C(i2cObj),
-mI2cAddr(i2cAddr)
+Imu::Imu(U8 i2cAddr, I2C* i2cObj)
+	:
+	mpI2C(i2cObj),
+	mI2cAddr(i2cAddr)
 {
 	setSleep(false);
-	printf("IMU at 0x%X initialized...\n",i2cAddr);
+	printf("IMU at 0x%X initialized...\n", i2cAddr);
 }
 
 Imu::~Imu()
@@ -30,43 +30,43 @@ Imu::~Imu()
 
 void Imu::setSleep(bool mode)
 {
-	U8 val = mode?0x40:0x00;
-	writeByte(val,MPU6050_PWR_MGMT_1);
+	U8 val = mode ? 0x40 : 0x00;
+	writeByte(val, MPU6050_PWR_MGMT_1);
 }
 
 U8 Imu::getSleepMode()
 {
 	U8 modeOut;
-	readByte(&modeOut,MPU6050_PWR_MGMT_1);
+	readByte(&modeOut, MPU6050_PWR_MGMT_1);
 	return modeOut;
 }
 
 
 void Imu::writeByte(U8 val, U8 fromReg)
 {
-	U8 buffer[] = {fromReg,val}; //First transmit the register address to write to, then the value
-	mpI2C->writeI2C(buffer,2,mI2cAddr);
+	U8 buffer[] = {fromReg, val}; //First transmit the register address to write to, then the value
+	mpI2C->writeI2C(buffer, 2, mI2cAddr);
 }
 
 void Imu::readByte(U8* buffer, U8 fromReg)
 {
-	mpI2C->writeI2C(&fromReg,1,mI2cAddr);
-	mpI2C->readI2C(buffer,1,mI2cAddr);
+	mpI2C->writeI2C(&fromReg, 1, mI2cAddr);
+	mpI2C->readI2C(buffer, 1, mI2cAddr);
 }
 
 signed short Imu::readShort(U8 fromReg)
 {
-	U8 wData[2] = {0,0};
-	readData(wData,2,fromReg);
-	signed short retVal = ((signed short)(wData[0]<<8|wData[1])); //Have to switch because of endian
+	U8 wData[2] = {0, 0};
+	readData(wData, 2, fromReg);
+	signed short retVal = ((signed short)(wData[0] << 8 | wData[1])); //Have to switch because of endian
 	return retVal;
 }
 
 
 void Imu::readData(U8* buffer, unsigned int readLength, U8 fromReg)
 {
-	mpI2C->writeI2C(&fromReg,1,mI2cAddr);
-	mpI2C->readI2C(buffer,readLength,mI2cAddr);
+	mpI2C->writeI2C(&fromReg, 1, mI2cAddr);
+	mpI2C->readI2C(buffer, readLength, mI2cAddr);
 }
 
 signed short Imu::getAccX(void)
@@ -114,3 +114,71 @@ Imu::accAll_t Imu::getAccAll(void)
 	return accel;
 }
 */
+
+/** Returns the angular position of the frame using a complementary filter. */
+double Imu::getPosition(double accAngleNow, double gyroVelocityNow, double Ts)
+{
+	//-------------- Tustin discretization ----------------//
+	const double acc_off = 0.84;  	// accel meass offset
+	const double tau = 0.5399;	// cut-off for the complementary filter
+
+	// Coefficients for the complementary equation
+	const double K1 = (2 * tau - Ts) / (2 * tau + Ts);
+	const double K2 = Ts / (2 * tau + Ts);
+
+	static double acc_angle[2] = {accAngleNow + acc_off, 0},
+	                             gyro_angle[2] = {gyroVelocityNow, gyroVelocityNow},
+	                             comp_angle[2] = {acc_angle[0], 0};
+
+	// Set old measurement data
+	acc_angle[1] = acc_angle[0];
+	gyro_angle[1] = gyro_angle[0];
+	gyro_angle[0] = gyroVelocityNow;
+	comp_angle[1] = comp_angle[0];
+
+	// Get angle from accel axis measurements
+	acc_angle[0] = accAngleNow + acc_off;
+
+	//Complementary equation using Tustin
+
+	comp_angle[0] = K1 * comp_angle[1] + K2 * (acc_angle[0] + acc_angle[1] + tau * gyro_angle[0] + tau * gyro_angle[1]);
+
+
+	//-------------- Euler discretization ----------------//
+	// const double acc_off 	= 0.82;   	// accel measurement offset, used to correct data to 0 pos.
+	//    const double tau 		= 0.1;   	// cut-off time-constant for the complementary filter
+	//    const double k 			= 0.99;
+
+	//    // Calculate the 2 constants of the complementary filter
+	//    const double K1 = (2*tau-Ts)/(2*tau+Ts);
+	//    const double K2 = Ts/(2*tau + Ts);
+
+	//    static double acc_angle[2]={accAngleNow + acc_off,0},
+	//        a=gyroVelocityNow,
+	//        gyro_angle[9]={a,a,a,a,a,a,a,a,a},
+	//        comp_angle[2]={acc_angle[0],0};
+
+	//    // Set old measurement data
+	//    acc_angle[1] = acc_angle[0];
+	//    for (int i=8; i>0; i--)
+	//      gyro_angle[i] = gyro_angle[i-1];
+	//    gyro_angle[0] = gyroVelocityNow;
+	//    comp_angle[1] = comp_angle[0];
+
+	//    // Get angle from accel axis measurements
+	//    acc_angle[0] = accAngleNow + acc_off;
+
+	//    // Get gyro angle from gyro measurement
+	//    static double sum=0;
+	//    for (int i=0; i<9; i++)
+	//      sum += gyro_angle[i];
+	//    gyro_angle[0] =  sum * 9 * Ts;
+	//    sum=0;
+
+	//    //Complementary equation using Tustin
+	//    //comp_angle[0] = K1*comp_angle[1] + K2*(acc_angle[0] + acc_angle[1] + tau*gyro_angle[0] + tau*gyro_angle[1]);
+	//    //Complementry equation using backward Euler
+	//    comp_angle[0] = k * (comp_angle[1] + gyroVelocityNow * Ts) + (1-k) * acc_angle[0];
+
+	return comp_angle[0];
+}
